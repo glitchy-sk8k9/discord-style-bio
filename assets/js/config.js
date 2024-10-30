d4556435114ebc46d98b627074ac37fceba32f02
Glitchy <sk8k9@proton.me>
2024-10-30 18:22:43 +0000

    made it my own

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,24 +1,16 @@
 const BADGES = [
   {
-    name: "HypeSquad Bravery", // Message that will appear when hovering over the badge
-    image: "assets/badges/hypesquad_bravery.svg",
-  },
-  {
-    name: "Subscriber since Jun 29, 2022",
-    image: "assets/badges/nitro.svg",
-  },
-  {
-    name: "Server boosting Jun 29, 2022",
-    image: "assets/badges/boost.svg",
-  },
-  {
-    name: "That's an example! Change in config.js", // Message that appears when you hover over it
+    name: "I have fun :)", // Message that appears when you hover over it
     image: "assets/badges/bug_hunter_level2.svg", // Icon (in .svg) of the badge that is in the "assets/badges" you choose
+  },
+  {
+    name: "Active Developer of Puppeteer",
+    image: "assets/badges/dev.svg"
   },
 ];
 
-const USER_ID = "277375966341496832"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
-const BACKGROUND_IMAGE = "assets/images/star.gif"; // Here is the background of the site, behind your profile
+const USER_ID = "1236667927944761396"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
+const BACKGROUND_IMAGE = "assets/images/codebg.png"; // Here is the background of the site, behind your profile
 const STATUS = {
   dnd: {
     name: "Do not disturb", // Discord Status
@@ -30,7 +22,7 @@ const STATUS = {
   },
   online: {
     name: "Online",
-    color: "#3ba55d",
+    color: "#3ba5b5",
   },
   offline: {
     name: "Offline",
